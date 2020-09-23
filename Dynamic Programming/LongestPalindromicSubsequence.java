db22ac6d67e8e8c6eaa5babd962a8f46fb7479c5
Raj Rawat <rawatraj995@gmail.com>
2020-09-23 11:05:20 +0530

    Update LongestPalindromicSubsequence.java

--- Dynamic Programming/LongestPalindromicSubsequence.java
+++ Dynamic Programming/LongestPalindromicSubsequence.java
@@ -1,4 +1,6 @@
-    static int lps(char ch[], int i, int j) {  
+   
+//Recursive Approach 
+static int lps(char ch[], int i, int j) {  
         if (i == j) { 
             return 1; 
         } 
@@ -11,3 +13,27 @@
         return Math.max(lps(ch, i, j - 1), lps(ch, i + 1, j)); 
     } 
   
+//DP Approach
+ static int lps(String ch) 
+    { 
+    int n = ch.length(); 
+    int i, j, cl; 
+    int L[][] = new int[n][n];  
+    for (i = 0; i < n; i++) 
+        L[i][i] = 1; 
+        for (cl=2; cl<=n; cl++) 
+        { 
+            for (i=0; i<n-cl+1; i++) 
+            { 
+                j = i+cl-1; 
+                if (ch.charAt(i) == ch.charAt(j) && cl == 2) 
+                L[i][j] = 2; 
+                else if (ch.charAt(i) == ch.charAt(j)) 
+                L[i][j] = L[i+1][j-1] + 2; 
+                else
+                L[i][j] = max(L[i][j-1], L[i+1][j]); 
+            } 
+        } 
+              
+        return L[0][n-1]; 
+    } 
