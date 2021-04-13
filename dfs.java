14129fa4ac75b96172782136ee40fc48a8d12bf1
Raj Rawat <rawatraj995@gmail.com>
2021-04-13 14:15:54 +0530

    Create dfs.java

---
+++ dfs.java
@@ -0,0 +1,6 @@
+public static void dfs(int node, int parent) {
+      for (int child : adj[node])
+            if (child != parent) {
+                dfs(child, node, consecutive);
+            }
+    }
