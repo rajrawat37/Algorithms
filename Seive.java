3ee88183a74217acdcf0340da6d0e24c067d7318
Raj Rawat <rawatraj995@gmail.com>
2021-06-08 06:29:57 +0530

    Update Seive.java

--- Seive.java
+++ Seive.java
@@ -1,4 +1,28 @@
- static int seive(int n){
+// 1
+
+ static ArrayList<Integer> sieve(int n) {
+
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime,true);
+
+        ArrayList<Integer> prime = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                prime.add(i);
+                for (int j = i + i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        return prime;
+    }
+
+// 2
+
+
+static int seive(int n){
        boolean [] compste= new boolean[n+1];
        for(int i=2;i*i<=n;i++){
             for(int j=i*i;j<=n && !compste[i];j+=i)
