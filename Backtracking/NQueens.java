5fd2136aeff66c0ad950582c29fe4af96f830a37
Raj Rawat <rawatraj995@gmail.com>
2022-03-22 06:59:08 +0530

    Update NQueens.java

--- Backtracking/NQueens.java
+++ Backtracking/NQueens.java
@@ -7,7 +7,7 @@
               int n = sc.nextInt();
               cL = new int[n]; //col
               rD = new int[n + n - 1]; //right diagonal
-              lD = new int[n + n - 1]; //;eft diagonal
+              lD = new int[n + n - 1]; //left diagonal
               ans = new char[n + 1][n + 1];
               for(int i=0;i<n;i++)
                      Arrays.fill(ans[i],'N');
@@ -29,8 +29,8 @@
               for(int col=0;col<n;col++){
                      if(cL[col]==0 && rD[row+col]==0 && lD[row-col+n-1]==0) {
                             cL[col] = 1;
-                            rD[row + col] = 1;
-                            lD[row - col + n - 1] = 1;
+                            rD[row + col] = 1; 
+                            lD[row - col + n - 1] = 1; //for indexing we take (row-col+n-1)  or (row+col ) , can be derived by pen-paper .
                             ans[row][col] = 'Q';
                             solve(n, row + 1);
                             cL[col] = 0;
